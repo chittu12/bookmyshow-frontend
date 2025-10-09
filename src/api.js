04452177283fb8907e09904cc3face10785d3acc
chittu12 <101640560+chittu12@users.noreply.github.com>
2025-10-09 10:11:04 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'http://192.168.0.103:8080/api' });
+const api = axios.create({ baseURL: 'https://bookmyshow-backend1-production.up.railway.app' });
 
 
 
